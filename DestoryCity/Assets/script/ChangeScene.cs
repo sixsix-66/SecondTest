4483b8b529514637ba0ae9d198affc2882e7adce
66sixsix <1455979688@qq.com>
2021-02-21 13:26:02 +0800

    特效拖拽

---
+++ DestoryCity/Assets/script/ChangeScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnClickto1()
+    {
+        SceneManager.LoadScene("Scene_1");
+    }
+    public void OnClickto2()
+    {
+        SceneManager.LoadScene("Scene_2");
+    }
+    /*
+    public void OnClickto3()
+    {
+        SceneManager.LoadScene("Scene_3");
+    }
+    public void OnClickto4()
+    {
+        SceneManager.LoadScene("Scene_4");
+    }
+    public void OnClickto5()
+    {
+        SceneManager.LoadScene("Scene_5");
+    }
+
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- DestoryCity/Assets/script/Drag.cs
+++ DestoryCity/Assets/script/Drag.cs
@@ -13,47 +13,92 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     private GameObject wupinobj;
     private Ray ray;
     private RaycastHit hit;
-    //private Vector3 colliderHitPoint;
+    private Vector3 colliderHitPoint;
+
+
+    GameObject guanghuan;
+    
+    LineRenderer LineRender;
+    float R=2;//半径
+    int N=200;   //描点个数
+    float width=4;  //圆的粗细
+    public Material CircleMaterial; //材质
+
+    
 
     void Start()
     {
+
         nowparent = GameObject.Find("Terrain");
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //生成3d物体
-        if (go != null)
-        {
-            wupinobj = Instantiate(go);
-            wupinobj.transform.SetParent(nowparent.transform);
-            Debug.Log("开始拖拽" + wupinobj.transform.parent);
-            wupinobj.transform.GetComponent<CanvasGroup>().blocksRaycasts = true;
-        }
+        //生成攻击范围圈
+        guanghuan = new GameObject("kong");
+        guanghuan.transform.SetParent(nowparent.transform);
+        guanghuan.AddComponent<LineRenderer>();
+        LineRender =guanghuan.GetComponent<LineRenderer>();
+        LineRender.positionCount = (N + 1);
+
+
+        Debug.Log("开始拖拽" + wupinobj.transform.parent);
+        
+        
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //将屏幕坐标转换为世界坐标
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       
         //使3D物体跟随鼠标移动
         if(Physics.Raycast(ray,out hit))
         {
             //将世界坐标转换为本地坐标
-            var colliderHitPoint = hit.collider.transform.InverseTransformPoint(hit.point);
-            wupinobj.transform.localPosition = new Vector3(colliderHitPoint.x, colliderHitPoint.y+1, colliderHitPoint.z);
+            colliderHitPoint = hit.collider.transform.InverseTransformPoint(hit.point);
+            
             Debug.Log("拖拽中");
-            Debug.Log(colliderHitPoint);
+
+            //绘制范围圈
+            drawCircle(colliderHitPoint);
+
+
         }
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        
+        //光环消失
+        Destroy(guanghuan);
+        Createobj();
         Debug.Log("拖拽结束" );
-        //实现特效伤害功能
+        
 
     }
 
-   
+    void Createobj()
+    {
+        //生成3d物体
+        wupinobj = Instantiate(go);
+        wupinobj.transform.SetParent(nowparent.transform);
+        wupinobj.transform.localPosition = new Vector3(colliderHitPoint.x, colliderHitPoint.y + 1, colliderHitPoint.z);
+        Destroy(wupinobj, 2.0f);
+    }
+
+    //绘制攻击范围
+    void drawCircle(Vector3 pos)
+    {
+        
+        LineRender.startWidth = width;
+        LineRender.material = CircleMaterial;
+
+        for (int i = 0; i < N + 1; i++)
+        {
+            float x = R * Mathf.Cos((360f / N * i) * Mathf.Deg2Rad) + pos.x; //确定x坐标
+            float z = R * Mathf.Sin((360f / N * i) * Mathf.Deg2Rad) + pos.z; //确定z坐标
+            LineRender.SetPosition(i,new Vector3(x, pos.y, z));
+            
+        }
+    }  
 }
